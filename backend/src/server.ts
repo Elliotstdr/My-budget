3f5ce60c99f02ba6a059d4a233c08aec30220ae8
Elliot Saint-André <elliot.standre@gmail.com>
2023-12-23 23:03:53 +0100

    [FIX] cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({origin: process.env.FRONTEND_URL}))
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
