94ab203046564d2b4b9bd5fe66a81b755032f4d2
Elliot Saint-André <elliot.standre@gmail.com>
2024-01-11 01:33:48 +0100

    [RESP] Accueil

--- frontend/src/Pages/Accueil/AccueilDesktop.tsx
+++ frontend/src/Pages/Accueil/AccueilDesktop.tsx
@@ -1,11 +1,9 @@
-import NavBar from "../../Components/NavBar/NavBar";
-import "./Accueil.scss";
 import image from "../../assets/tirelire-blue.png";
 import { useDispatch, useSelector } from "react-redux";
-import { FaUserCog } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import Box from "../../Utils/Box/Box";
-import { RiMoneyEuroCircleLine, RiBankFill } from "react-icons/ri";
+import { CiImport } from "react-icons/ci";
+import { TbHandClick } from "react-icons/tb";
 import { rangeTiretDate } from "../../Services/functions";
 import { fetchPost } from "../../Services/api";
 import { useEffect } from "react";
@@ -13,11 +11,14 @@ import {
   PieChart, Pie, Cell, LabelList
 } from 'recharts';
 import { UPDATE_DASHBOARD } from "../../Store/Reducers/dashboardReducer";
-import AccueilDesktop from "./AccueilDesktop";
+import Header from "../../Components/Header/Header";
+import Accueil1 from "../../assets/Accueil3.jpg"
+import Accueil2 from "../../assets/Accueil2.png"
+import Bouton from "../../Utils/Bouton/Bouton";
 
-const Accueil = () => {
+const AccueilDesktop = () => {
   const navigate = useNavigate()
-  const auth = useSelector((state: RootState) => state.auth);
+  // const auth = useSelector((state: RootState) => state.auth);
   const dashboard = useSelector((state: RootState) => state.dashboard);
   const dispatch = useDispatch();
   const updateDashboard = (value: Partial<DashboardState>) => {
@@ -52,48 +53,56 @@ const Accueil = () => {
 
   return (
     <>
-      <div className="accueil page">
-        <div className="blue--zone">
-          <div className="blue--zone__left">
-            <img src={image} alt="background home" />
-            <div className="blue--zone__left__text">
-              <span className="main">{`Bonjour ${auth.userConnected?.username}`}</span>
-              <span className="sub">Heureux de te revoir !</span>
+      <Header title="Accueil"></Header>
+      <div className="accueil__desktop page">
+        <div
+          className="accueil__desktop__bandeau"
+          style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${Accueil1})` }}
+        >
+          <div className="accueil__desktop__bandeau__encart">
+            <img className="logo" src={image} alt="" />
+            <div className="block">
+              <div>Bonjour Elliot,</div>
+              <span>Heureux de te revoir !</span>
             </div>
           </div>
-          <div
-            className="blue--zone__right"
-            onClick={() => navigate("/parameters")}
-          >
-            <FaUserCog></FaUserCog>
+        </div>
+        <div className="accueil__desktop__body">
+          <div className="white--zone">
+            <div className="sentence">Fais le point sur tes dépenses du mois </div>
+            <span className="main">{`${dashboard.newExpense}€`}</span>
+            <span className="sub">dépensés ce mois ci !</span>
+            <Bouton
+              btnTexte="Accéder aux statistiques"
+              btnAction={() => navigate("/statistics")}
+              color="pink"
+            ></Bouton>
           </div>
-        </div>
-        <div className="white--zone">
-          <span className="main">{`${dashboard.newExpense}€`}</span>
-          <span className="sub">dépensés ce mois ci !</span>
-        </div>
-        <div className="accueil__body">
-          <div className="first">
+          <div className="accueil__desktop__body__right">
             <div className="title">Accès rapide</div>
-            <div className="accueil__body__boxes">
+            <div className="accueil__desktop__body__right__boxes">
               <Box
-                text="Salaire brut/net"
-                icon={<RiMoneyEuroCircleLine></RiMoneyEuroCircleLine>}
-                action={() => navigate("/tools/salary")}
-                width="8rem"
+                text="Import fichier"
+                icon={<CiImport></CiImport>}
+                action={() => navigate("/import/csv")}
+                width="100%"
+                height="120px"
               ></Box>
               <Box
-                text="Capacité d'emprunt"
-                icon={<RiBankFill></RiBankFill>}
-                action={() => navigate("/tools/loan")}
-                width="8rem"
+                text="Saisie manuelle"
+                icon={<TbHandClick></TbHandClick>}
+                action={() => navigate("/import/manual")}
+                width="100%"
+                height="120px"
               ></Box>
             </div>
           </div>
+        </div>
+        <div className="accueil__desktop__bottom" style={{ backgroundImage: `url(${Accueil2})` }}>
           <div className="second">
-            <div className="title">Plus grosses dépenses du mois</div>
+            <div className="title">Ta plus grosse dépense du mois :</div>
             {dashboard.data ?
-              <div className="accueil__body__pie">
+              <div className="accueil__desktop__bottom__pie">
                 <PieChart width={300} height={300}>
                   <Pie
                     dataKey="value"
@@ -115,7 +124,7 @@ const Accueil = () => {
                     }
                   </Pie>
                 </PieChart>
-                {dashboard.data && <div style={{ maxWidth: "40%" }}>
+                {dashboard.data && <div className="legend">
                   {`Tu as dépensé ${dashboard.maxExpensePercentage}% de ton budget dans le poste de dépense "${dashboard.data[0].name}"`}
                 </div>}
               </div>
@@ -127,10 +136,8 @@ const Accueil = () => {
           </div>
         </div>
       </div>
-      <AccueilDesktop></AccueilDesktop>
-      <NavBar></NavBar>
     </>
   );
 };
 
-export default Accueil;
+export default AccueilDesktop;
